99e872dac454ec1b4d7a4ac6c57b6b247cff9f98
Louis Devillez <louis.devillez@gmail.com>
2020-07-20 16:07:59 +0200

    open the file

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import json
+import codecs
+import sys
+
+name = 'data.json'
+
+data = {}
+
+try:
+  # We open the file
+  with codecs.open(name,'r','utf-8') as json_file:
+    # We load the JSON
+    data = json.load(json_file)
+except IOError:
+    sys.exit('The file ' + name + ' is not found!')
+
